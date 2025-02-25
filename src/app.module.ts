720dfba514e3d958ce1ede5c750bfdc2ac18fda4
Dieguidev <diegogaraycullas@gmail.com>
2025-02-24 21:34:30 -0500

    Agrega entidades y DTOs para User, Follow y Publication. Implementa módulos y servicios correspondientes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FollowModule } from './follow/follow.module';
+import { PublicationModule } from './publication/publication.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [FollowModule, PublicationModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
